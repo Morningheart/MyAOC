beb05835129f63bf89bf58dee63fe2354b8e7313
Morningheart <pierre.pagin@gmail.com>
2023-11-21 00:45:00 +0100

    :sparkles: Year 2021 Day 06 Essai de faire Ex 2

--- src/main/java/year2021/day06/codes/LanternFish.java
+++ src/main/java/year2021/day06/codes/LanternFish.java
@@ -1,6 +1,9 @@
 package year2021.day06.codes;
 
+import lombok.Getter;
+
 class LanternFish {
+    @Getter
     private int age;
     private boolean hasProducedNewFish;
 
@@ -8,7 +11,7 @@ class LanternFish {
         this.age = age;
     }
 
-    public void age() {
+    public void oneDayPasses() {
         hasProducedNewFish = (age == 0); // Indicating if the fish has produced a new fish
         age = age == 0 ? 6 : --age;
     }
@@ -22,3 +25,7 @@ class LanternFish {
         return String.valueOf(age);
     }
 }
+
+// Initial state: 3,4,3,1,2
+// After  1 day:  2,3,2,0,1
+// After  2 days: 1,2,1,6,0,8
--- src/main/java/year2021/day06/codes/Main.java
+++ src/main/java/year2021/day06/codes/Main.java
@@ -3,7 +3,9 @@ package year2021.day06.codes;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,10 +18,14 @@ public class Main {
         // List<Integer> initialFishAges = parser("src/main/java/year2021/day06/inputs/input.txt");
 
         // Simulating lanternfish growth
-        List<LanternFish> lanternfish = simulateLanternfish(initialFishAges, days);
+        List<LanternFish> lanternfishPart1 = simulateLanternfishEx1(initialFishAges, 80);
+        List<LanternFish> lanternfishPart2Try1 = simulateLanternfishEx2Essai1(initialFishAges, days);
+        List<List<LanternFish>> lanternfishPart2Try2 = simulateLanternfishEx2Essai2(initialFishAges, days);
 
         // Displaying the result
-        System.out.println("After " + days + " days, there are " + lanternfish.size() + " lanternfish.");
+        System.out.println("Part 1 : After " + days + " days, there are " + lanternfishPart1.size() + " lanternfish.");
+        System.out.println("Part 2 - Try 1 : After " + days + " days, there are " + lanternfishPart2Try1.size() + " lanternfish.");
+        System.out.println("Part 2 - Try 1 : After " + days + " days, there are " + countTotalLanternfish(lanternfishPart2Try2).toString() + " lanternfish.");
     }
 
     private static List<Integer> parser(String filename) throws IOException{
@@ -31,14 +37,14 @@ public class Main {
         }
     }
 
-    private static List<LanternFish> simulateLanternfish(List<Integer> initialAges, int days) {
+    private static List<LanternFish> simulateLanternfishEx1(List<Integer> initialAges, int days) {
         List<LanternFish> lanternfish = initialAges.stream()
                 .map(LanternFish::new)
                 .collect(Collectors.toList());
 
         for (int day = 1; day <= days; day++) {
             List<LanternFish> newLanternfish = lanternfish.stream()
-                    .peek(LanternFish::age)
+                    .peek(LanternFish::oneDayPasses)
                     .filter(LanternFish::hasProducedNewFish)
                     .map(fish -> new LanternFish(8))
                     .toList();
@@ -50,4 +56,60 @@ public class Main {
 
         return lanternfish;
     }
+
+    private static List<LanternFish> simulateLanternfishEx2Essai1(List<Integer> initialAges, int days) {
+    List<LanternFish> lanternfish = initialAges.stream()
+            .map(LanternFish::new)
+            .collect(Collectors.toCollection(LinkedList::new));
+
+        for (int day = 1; day <= days; day++) {
+        List<LanternFish> newLanternfish = lanternfish.stream()
+                .peek(LanternFish::oneDayPasses)
+                .filter(LanternFish::hasProducedNewFish)
+                .map(fish -> new LanternFish(8))
+                .collect(Collectors.toCollection(LinkedList::new));
+
+        lanternfish.addAll(newLanternfish); // Adding the new fish to the total list
+
+        // System.out.println("Day " + day + ": " + lanternfish.size() + " lanternfish.");
+    }
+
+        return lanternfish;
+}
+
+    private static List<List<LanternFish>> simulateLanternfishEx2Essai2(List<Integer> initialAges, int days) {
+        List<List<LanternFish>> lanternfish = new ArrayList<>();
+        lanternfish.add(initialAges.stream()
+                .map(LanternFish::new)
+                .collect(Collectors.toList()));
+
+        List<List<LanternFish>> updatedListOfLanternfish = new ArrayList<>(lanternfish);
+        for (int iteration = 1; iteration <= days; iteration++) {
+
+            for (List<LanternFish> laternfishOfTheDay : updatedListOfLanternfish) {
+
+                List<LanternFish> oneDayPassed = simulateLanternfishEx1(laternfishOfTheDay
+                        .stream()
+                        .map(LanternFish::getAge)
+                        .collect(Collectors.toList()), 1);
+
+                List<LanternFish> updatedLanternfish = new ArrayList<>(oneDayPassed.subList(0, laternfishOfTheDay.size()));
+                List<LanternFish> newLanternfish = new ArrayList<>(laternfishOfTheDay.subList(laternfishOfTheDay.size(), oneDayPassed.size()));
+
+                // FF
+            }
+            System.out.println("Day " + iteration + ": " + lanternfish.get(iteration).size() + " lanternfish.");
+        }
+
+        return lanternfish;
+    }
+
+    private static Double countTotalLanternfish(List<List<LanternFish>> lanternfish) {
+        double total = 0.0;
+        for (List<LanternFish> day : lanternfish) {
+            total += day.size();
+        }
+        return total;
+    }
+
 }
